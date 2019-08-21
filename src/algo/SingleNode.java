e8110e42140612093e70817c8b9a07fa154e6347
ivartb <ivart07@mail.ru>
2019-08-21 15:18:49 +0300

    Add graph visualisation in .gfa format showing the results of FMT classification

--- src/algo/SingleNode.java
+++ src/algo/SingleNode.java
@@ -13,11 +13,28 @@ public class SingleNode {
     boolean visited;
     boolean changed;
 
+    public Color color;
+
+    public enum Color {RED, GREEN, BLUE, GREY, YELLOW, BLACK}
+
 
     public SingleNode(String sequence, int id, boolean isGeneNode) {
         this.sequence = sequence;
         this.id = id;
         this.isGeneNode = isGeneNode;
+        this.color = null;
+
+        this.deleted = false;
+        this.visited = false;
+        this.changed = false;
+        this.neighbors = new ArrayList<SingleNode>();
+    }
+
+    public SingleNode(String sequence, int id, Color color) {
+        this.sequence = sequence;
+        this.id = id;
+        this.isGeneNode = false;
+        this.color = color;
 
         this.deleted = false;
         this.visited = false;
