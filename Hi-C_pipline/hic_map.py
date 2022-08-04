798a7871c66ac44c76c9c7ea69d6dc0a0635b640
Shostina <shostina77@gmail.com>
2022-08-04 19:54:30 +0300

    Add test and test data

---
+++ Hi-C_pipline/hic_map.py
@@ -0,0 +1,21 @@
+#-*- coding: UTF-8 -*-
+from sys import argv
+
+script, suffix, name = argv
+
+data = dict()
+
+i = 0
+for line in open(suffix+'/'+name):
+  l = line.strip().split()
+  k = tuple(sorted((l[2], l[6])))
+  data[k] = data.get(k, 0) + 1
+  i += 1
+  if i % 1000000 == 0:
+    print(i, flush=True)
+
+f = open(suffix +"/" + "hic_map.txt", "w")
+print("v1", "v2", "hic_w", sep="\t", file=f)
+for k, v in data.items():
+  print(k[0], k[1], v // 2, sep="\t", file=f)
+f.close()
